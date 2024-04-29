380e0cae35ae2a6ab8669c9e026e65fc8109408f
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2024-04-29 15:02:11 +0530

    Banner refreshing feature added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,13 +4,18 @@ import axios from '../../axios'
 import './Banner.css'
 function Banner() {
   const [movie, setMovie] = useState([])
-        useEffect(() =>{
-          axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((Response)=>{
-            const randomIndex = Math.floor(Math.random() * Response.data.results.length)
-            setMovie(Response.data.results[randomIndex])
-            console.log(Response.data.results)
-          })
-     },[]);
+        useEffect(() => {
+    const intervalId = setInterval(() => {
+      axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((Response)=>{
+        const randomIndex = Math.floor(Math.random() * Response.data.results.length)
+        setMovie(Response.data.results[randomIndex])
+        console.log(intervalId)
+      })
+    }, 10000) // Change the banner every 10 seconds
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(intervalId)
+  }, [])  
 
      
   return (
